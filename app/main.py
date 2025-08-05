56b908e7f8ab64de1f4bafde2b34adc2401cda3f
Yuu08-gif <masaru19990827@gmail.com>
2025-08-06 00:43:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,31 @@
 import os
 import discord
 import dotenv
-
 from server import server_thread
 
 dotenv.load_dotenv()
 
 TOKEN = os.environ.get("TOKEN")
+print(f"TOKEN LOADED? {'Yes' if TOKEN else 'No'}")
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 client = discord.Client(intents=intents)
 
 @client.event
+async def on_ready():
+    print(f'✅ Bot logged in as {client.user}')
+
+@client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-# Koyeb用 サーバー立ち上げ
+# Webサーバー起動（別スレッド）
 server_thread()
+
+# Bot起動
 client.run(TOKEN)
