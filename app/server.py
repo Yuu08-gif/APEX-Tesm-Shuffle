62b8f1ca6a6866614f5daa1298a8c57fb2bb1bb6
Yuu08-gif <masaru19990827@gmail.com>
2025-08-06 17:01:41 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+"""
 from threading import Thread
 
 from fastapi import FastAPI
@@ -15,3 +16,22 @@ def start():
 def server_thread():
 	t = Thread(target=start)
 	t.start()
+"""
+
+import asyncio
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Server is Online."}
+
+async def start():
+    config = uvicorn.Config(app, host="0.0.0.0", port=8080)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+def server_thread():
+    asyncio.run(start())  # 非同期処理を使ってサーバーを起動
