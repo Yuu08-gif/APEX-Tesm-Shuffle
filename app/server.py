187ae27d3cbf84b5aa88b3d2bbd2eeb762a95110
Yuu08-gif <masaru19990827@gmail.com>
2025-08-06 17:09:35 +0900

    Update server.py

--- app/server.py
+++ app/server.py
@@ -29,6 +29,10 @@ app = FastAPI()
 async def root():
     return {"message": "Server is Online."}
 
+@app.head("/")  # HEAD リクエストにも対応
+async def head_root():
+    return {"message": "Server is Online."}
+
 def start():
     port = int(os.environ.get("PORT", 8080))  # ポートを環境変数から取得
     uvicorn.run(app, host="0.0.0.0", port=port)
