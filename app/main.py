d7d8856bed4f59c5392ee2429f4d63432c6a4981
Yuu08-gif <masaru19990827@gmail.com>
2025-08-11 15:07:49 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,9 +24,9 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-@client.command(name="ping", description="pingを返します")
+@client.command(name='ping', description='pingを返します')
 async def ping(ctx: discord.ApplicationContext):
-    await ctx.respond("pong")
+    await ctx.respond('pong')
 
 # Webサーバー起動（別スレッド）
 server_thread()
